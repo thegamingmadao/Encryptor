70a13087b633017142bef0060911fb3ba106f872
thegamingmadao <thegamingmadao@gmail.com>
2018-05-19 20:24:55 +0300

    test commit

--- encryptor/models.py
+++ encryptor/models.py
@@ -1,3 +1,4 @@
+# a comment in an existing file to test git functionality
 from django.db import models
 
 # Create your models here.
---
+++ testfile.py
@@ -0,0 +1,1 @@
+# this is a test file created to test the basic functionalities of git 
